c9829a5d39e9bbe9e27a0dae64809c445599e5cd
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-06-19 14:46:16 +0700

    membuat int main

--- ioFile.cpp_205/ioFile.cpp_205.cpp
+++ ioFile.cpp_205/ioFile.cpp_205.cpp
@@ -3,4 +3,15 @@
 #include <string>
 using namespace std;
 
+int main() {
+	string baris;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+
+}
+
 
