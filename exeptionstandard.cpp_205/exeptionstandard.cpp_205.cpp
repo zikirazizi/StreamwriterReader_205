ebb52cfcccf9966c9483778cf48498ec9e190401
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-06-19 15:53:14 +0700

    awalan

--- exeptionstandard.cpp_205/exeptionstandard.cpp_205.cpp
+++ exeptionstandard.cpp_205/exeptionstandard.cpp_205.cpp
@@ -5,23 +5,3 @@
 //untuk obyek array yang akan kita gunakan 
 using namespace std;
 
-int main()
-{
-	cout << "Awal Program" << endl; //penanda 1:...
-	try {
-		array<int, 3> data = { 1, 2, 3 };
-		//pesan array integer 3 elemen
-		cout << data.at(4) << endl;
-		//memanggil array elemen ke 5
-	}
-	catch (exception& e) {
-		//penangkap menngunakan obyek exception
-		cout << e.what() << endl;
-		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
-	}
-
-	cout << "Baris Program Yang Terakhir" << endl;
-	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
-	return 0;
-}
-	
