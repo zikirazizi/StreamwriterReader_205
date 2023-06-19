32d8c5ca281332505d415da251d24ec9bb191b4e
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-06-19 14:51:50 +0700

    getline

--- ioFile.cpp_205/ioFile.cpp_205.cpp
+++ ioFile.cpp_205/ioFile.cpp_205.cpp
@@ -17,6 +17,9 @@ int main() {
 	while (true) {
 		cout << "- ";
 		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda masukkan karakter q
+		if (baris == "a") break;
 	}
 
 
