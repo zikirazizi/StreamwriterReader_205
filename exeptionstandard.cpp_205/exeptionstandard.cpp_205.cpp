3df12c23baf5005118635cd3b3ee281f0b89989e
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-06-19 15:52:46 +0700

    membuat cacth

--- exeptionstandard.cpp_205/exeptionstandard.cpp_205.cpp
+++ exeptionstandard.cpp_205/exeptionstandard.cpp_205.cpp
@@ -14,4 +14,14 @@ int main()
 		cout << data.at(4) << endl;
 		//memanggil array elemen ke 5
 	}
+	catch (exception& e) {
+		//penangkap menngunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+	}
+
+	cout << "Baris Program Yang Terakhir" << endl;
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+	return 0;
+}
 	
